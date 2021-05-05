8cb099698316cb76616e3de6a4ce7154f2c6a20f
chichaparadise <vlad.krapivnitsky@gmail.com>
2021-05-06 00:59:55 +0300

    fixed importing pathes

---
+++ lab2/serializer/main.py
@@ -0,0 +1,7 @@
+import sys
+from src.Factory import SerializerFactory as sf
+
+
+a = sf.SerializerFactory('')
+
+print(sys.path)
--- lab2/serializer/src/packer/packer.py
+++ lab2/serializer/src/packer/packer.py
@@ -4,20 +4,20 @@ import builtins
 import inspect
 import sys
 
-class Packer():
 
+class Packer:
     def __init__(self):
         self.mod = importlib.import_module(__name__)
 
     def pack_instance(self, obj):
         obj_dict = {}
         print(obj)
-        obj_dict['class'] = obj.__class__
-        obj_dict['dict'] = obj.__dict__
+        obj_dict["class"] = obj.__class__
+        obj_dict["dict"] = obj.__dict__
         return obj_dict
 
     def is_instance(self, obj):
-        if not hasattr(obj, '__dict__'):
+        if not hasattr(obj, "__dict__"):
             return False
         if inspect.isroutine(obj):
             return False
@@ -26,15 +26,17 @@ class Packer():
         if inspect.ismodule(obj):
             return False
         else:
-            if obj.__class__.__name__ in dict(inspect.getmembers(self.mod, inspect.isclass)):
+            if obj.__class__.__name__ in dict(
+                inspect.getmembers(self.mod, inspect.isclass)
+            ):
                 return True
             else:
                 return False
 
     def pack_instance(self, obj):
         obj_dict = {}
-        obj_dict['class'] = obj.__class__
-        obj_dict['dict'] = obj.__dict__
+        obj_dict["class"] = obj.__class__
+        obj_dict["dict"] = obj.__dict__
         return obj_dict
 
     def get_closure_globs(self, code_obj, globs):
@@ -44,22 +46,24 @@ class Packer():
                 mod = importlib.import_module(__name__)
             for name in code_obj.co_names:
                 if name in dir(mod):
-                        globs[name] = getattr(mod, name)
+                    globs[name] = getattr(mod, name)
                 elif name in dir(builtins):
                     globs[name] = getattr(builtins, name)
 
     def pack_class(self, obj):
         obj_dict = {}
-        obj_dict['__name__'] = obj.__name__
-        obj_dict['__bases__'] = tuple([base for base in obj.__bases__ if not base is object])
-        obj_dict['__dict__'] = dict(obj.__dict__)
+        obj_dict["__name__"] = obj.__name__
+        obj_dict["__bases__"] = tuple(
+            [base for base in obj.__bases__ if not base is object]
+        )
+        obj_dict["__dict__"] = dict(obj.__dict__)
         return obj_dict
 
     def get_codeobject_attrs(self, obj):
         if isinstance(obj, types.CodeType):
             obj_dict = {}
             for key in dir(obj):
-                if key.startswith('co_'):
+                if key.startswith("co_"):
                     value = obj.__getattribute__(key)
                     obj_dict[key] = value
             return obj_dict
@@ -67,21 +71,21 @@ class Packer():
     def pack_function(self, obj):
         obj_dict = {}
         attrs = {}
-        attrs['__name__'] = obj.__qualname__
-        attrs['__defaults__'] = obj.__defaults__
-        attrs['__closure__'] = obj.__closure__  
-        attrs['__code__'] = obj.__code__
+        attrs["__name__"] = obj.__qualname__
+        attrs["__defaults__"] = obj.__defaults__
+        attrs["__closure__"] = obj.__closure__
+        attrs["__code__"] = obj.__code__
         global_ns = {}
         self.get_closure_globs(obj.__code__, global_ns)
-        obj_dict['__globals__'] = global_ns
-        obj_dict['attributes'] = attrs
+        obj_dict["__globals__"] = global_ns
+        obj_dict["attributes"] = attrs
         return obj_dict
 
     def pack_builtinfunction(self, obj):
         obj_dict = {}
-        obj_dict['type'] = 'builtinfunction'
-        obj_dict['module'] = obj.__module__
-        obj_dict['attributes'] = {'__name__' : obj.__name__}
+        obj_dict["type"] = "builtinfunction"
+        obj_dict["module"] = obj.__module__
+        obj_dict["attributes"] = {"__name__": obj.__name__}
         return obj_dict
 
     def pack(self, obj):
@@ -89,80 +93,80 @@ class Packer():
 
         if type(obj) in (int, float, str, bool):
             if type(obj) == int:
-                obj_dict['type'] = 'int'
-                obj_dict['data'] = obj
+                obj_dict["type"] = "int"
+                obj_dict["data"] = obj
                 return obj_dict
             elif type(obj) == float:
-                obj_dict['type'] = 'float'
-                obj_dict['data'] = obj
+                obj_dict["type"] = "float"
+                obj_dict["data"] = obj
                 return obj_dict
             elif type(obj) == bool:
-                obj_dict['type'] = 'bool'
-                obj_dict['data'] = obj
+                obj_dict["type"] = "bool"
+                obj_dict["data"] = obj
                 return obj_dict
             elif type(obj) == str:
-                obj_dict['type'] = 'str'
-                obj_dict['data'] = obj
+                obj_dict["type"] = "str"
+                obj_dict["data"] = obj
                 return obj_dict
 
         if type(obj) in (dict, list, tuple, set, frozenset):
             if isinstance(obj, dict):
-                obj_dict['type'] = 'dict'
-                obj_dict['data'] = {key : self.pack(val) for key, val in obj.items()}
+                obj_dict["type"] = "dict"
+                obj_dict["data"] = {key: self.pack(val) for key, val in obj.items()}
                 return obj_dict
             if isinstance(obj, list):
-                obj_dict['type'] = 'list'
-                obj_dict['data'] = [self.pack(el) for el in obj]
+                obj_dict["type"] = "list"
+                obj_dict["data"] = [self.pack(el) for el in obj]
                 return obj_dict
             if isinstance(obj, tuple):
-                obj_dict['type'] = 'tuple'
-                obj_dict['data'] = [self.pack(el) for el in obj]
+                obj_dict["type"] = "tuple"
+                obj_dict["data"] = [self.pack(el) for el in obj]
                 return obj_dict
             if isinstance(obj, list):
-                obj_dict['type'] = 'set'
-                obj_dict['data'] = [self.pack(el) for el in obj]
+                obj_dict["type"] = "set"
+                obj_dict["data"] = [self.pack(el) for el in obj]
                 return obj_dict
             if isinstance(obj, frozenset()):
-                obj_dict['type'] = 'frozenset'
-                obj_dict['data'] = [self.pack(el) for el in obj]
+                obj_dict["type"] = "frozenset"
+                obj_dict["data"] = [self.pack(el) for el in obj]
                 return obj_dict
 
         if isinstance(obj, bytes):
-            obj_dict['type'] = 'bytes'
-            obj_dict['data'] = [byte for byte in obj]
+            obj_dict["type"] = "bytes"
+            obj_dict["data"] = [byte for byte in obj]
             return obj_dict
 
         if isinstance(obj, bytearray):
-            obj_dict['type'] = 'bytearray'
-            obj_dict['data'] = [byte for byte in obj]
+            obj_dict["type"] = "bytearray"
+            obj_dict["data"] = [byte for byte in obj]
             return obj_dict
 
         if isinstance(obj, types.CodeType):
-            obj_dict['type'] = 'codeobject'
-            obj_dict['data'] = self.pack(self.get_codeobject_attrs(obj))
+            obj_dict["type"] = "codeobject"
+            obj_dict["data"] = self.pack(self.get_codeobject_attrs(obj))
             return obj_dict
-    
+
         if isinstance(obj, types.FunctionType):
-            obj_dict['type'] = 'function'
-            obj_dict['data'] = self.pack(self.pack_function(obj))
+            obj_dict["type"] = "function"
+            obj_dict["data"] = self.pack(self.pack_function(obj))
             return obj_dict
 
         if isinstance(obj, types.BuiltinFunctionType):
-            obj_dict['type'] = 'builtinfunction'
-            obj_dict['data'] = self.pack(self.pack_builtinfunction(obj))
+            obj_dict["type"] = "builtinfunction"
+            obj_dict["data"] = self.pack(self.pack_builtinfunction(obj))
             return obj_dict
 
         if isinstance(obj, types.CellType):
-                obj_dict['type'] = 'celltype'
-                obj_dict['data'] = self.pack(obj.cell_contents)
-                return obj_dict
+            obj_dict["type"] = "celltype"
+            obj_dict["data"] = self.pack(obj.cell_contents)
+            return obj_dict
 
         if isinstance(obj, type):
-            obj_dict['type'] = 'class'
-            obj_dict['data'] = self.pack(self.pack_class(obj))
+            obj_dict["type"] = "class"
+            obj_dict["data"] = self.pack(self.pack_class(obj))
             return obj_dict
 
         if self.is_instance(obj):
-            obj_dict['type'] = 'instance'
-            obj_dict['data'] = self.pack(self.pack_instance(obj))
+            obj_dict["type"] = "instance"
+            obj_dict["data"] = self.pack(self.pack_instance(obj))
             return obj_dict
