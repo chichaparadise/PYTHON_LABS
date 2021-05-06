13aa6bd34cb62590f850d8fdb4e06c5a53d22a69
chichaparadise <vlad.krapivnitsky@gmail.com>
2021-05-06 14:38:03 +0300

    added properties to Serializer

--- lab2/serializer/src/Serializer.py
+++ lab2/serializer/src/Serializer.py
@@ -3,9 +3,29 @@ from src.Factory import SerializerFactory as sf
 class Serializer():
 
     def __init__(self, path, extension='json'):
-        self.extension = extension
-        self.factory = sf.SerializerFactory(path)
-        self.serializer = self.factory.create_serializer(extension)
+        self._path = path
+        self._extension = extension
+        self.factory = sf.SerializerFactory(self._path)
+        self.serializer = self.factory.create_serializer(self._extension)
+
+    @property
+    def path(self):
+        return self._path
+
+    @path.setter
+    def path(self, path):
+        self._path = path
+        self.factory = sf.SerializerFactory(self._path)
+        self.serializer = self.factory.create_serializer(self._path)
+
+    @property
+    def extension(self):
+        return self._extension
+
+    @extension.setter
+    def extension(self, extension):
+        self._extension = extension
+        self.serializer = self.factory.create_serializer(self._extension)
 
     def dump(self, obj):
         self.serializer.dump(obj)
