9e00dc094ef16b5451fd3e909bc4100c0919e2f7
chichaparadise <vlad.krapivnitsky@gmail.com>
2021-05-30 22:56:44 +0300

    minor fixes

--- lab3/project/jdmcars/cars/models.py
+++ lab3/project/jdmcars/cars/models.py
@@ -1,7 +1,13 @@
 from django.db import models
+from django.urls import reverse
+
 from .db_models.offer import Offer
 from .db_models.statistics import Statistics
 from .db_models.userprofile import UserProfile
 from .db_models.image import Image
 from .db_models.model import Model
 from .db_models.mark import Mark
+
+
+def get_offer_url(obj, urlpattern_name, model_name):
+    return reverse
